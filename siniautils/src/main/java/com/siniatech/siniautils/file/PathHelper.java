d529caaccb5633fc453470ef5a301627f30120e1
Si Fraser <si@ubuntu-desktop.(none)>
2012-03-03 12:37:59 +0000

    reading helper

--- siniautils/src/main/java/com/siniatech/siniautils/file/PathHelper.java
+++ siniautils/src/main/java/com/siniatech/siniautils/file/PathHelper.java
@@ -2,9 +2,11 @@ package com.siniatech.siniautils.file;
 
 import static java.nio.file.Files.createFile;
 import static java.nio.file.Files.exists;
+import static java.nio.file.Files.newBufferedReader;
 import static java.nio.file.Files.newBufferedWriter;
 import static java.nio.file.Files.newInputStream;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,33 +18,47 @@ import java.security.NoSuchAlgorithmException;
 
 public class PathHelper {
 
-    static public Path createFileWithContents( String pathString, String contents ) throws IOException {
-        Path path = FileSystems.getDefault().getPath( pathString );
-        if ( exists( path ) ) {
-            throw new IOException( "File " + pathString + " already exists." );
-        } else {
-            createFile( path );
-        }
-        BufferedWriter out = newBufferedWriter( path, Charset.defaultCharset() );
-        out.write( contents );
-        out.close();
-        return path;
-    }
+	static public Path createFileWithContents(String pathString, String contents)
+			throws IOException {
+		Path path = FileSystems.getDefault().getPath(pathString);
+		if (exists(path)) {
+			throw new IOException("File " + pathString + " already exists.");
+		} else {
+			createFile(path);
+		}
+		BufferedWriter out = newBufferedWriter(path, Charset.defaultCharset());
+		out.write(contents);
+		out.close();
+		return path;
+	}
 
-    static public String sha1( Path path ) throws IOException, NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance( "SHA1" );
-        InputStream is = newInputStream( path );
-        byte[] dataBytes = new byte[1024];
-        int nread = 0;
-        while ( ( nread = is.read() ) != -1 ) {
-            md.update( dataBytes, 0, nread );
-        }
-        byte[] mdbytes = md.digest();
-        StringBuffer sb = new StringBuffer( "" );
-        for ( int i = 0; i < mdbytes.length; i++ ) {
-            sb.append( Integer.toString( ( mdbytes[i] & 0xff ) + 0x100, 16 ).substring( 1 ) );
-        }
+	static public String getFileContents(Path file) throws IOException {
+		StringBuffer sb = new StringBuffer();
+		BufferedReader in = newBufferedReader(file, Charset.defaultCharset());
+		String line;
+		while ((line = in.readLine()) != null) {
+			sb.append(line);
+		}
+		in.close();
+		return sb.toString();
+	}
 
-        return sb.toString();
-    }
+	static public String sha1(Path path) throws IOException,
+			NoSuchAlgorithmException {
+		MessageDigest md = MessageDigest.getInstance("SHA1");
+		InputStream is = newInputStream(path);
+		byte[] dataBytes = new byte[1024];
+		int nread = 0;
+		while ((nread = is.read()) != -1) {
+			md.update(dataBytes, 0, nread);
+		}
+		byte[] mdbytes = md.digest();
+		StringBuffer sb = new StringBuffer("");
+		for (int i = 0; i < mdbytes.length; i++) {
+			sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16)
+					.substring(1));
+		}
+
+		return sb.toString();
+	}
 }
