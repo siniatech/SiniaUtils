456178e932e62d0231220648bfc315975d63e7f6
Si Fraser <simon@siniatech.com>
2012-04-14 15:03:52 +0100

    annotation

---
+++ siniautils/src/main/java/com/siniatech/siniautils/annotations/DefaultForTesting.java
@@ -0,0 +1,9 @@
+package com.siniatech.siniautils.annotations;
+
+/**
+ * Used to mark a method that would normally be private but has been given
+ * default access to allow unit testing.
+ */
+public @interface DefaultForTesting {
+
+}
