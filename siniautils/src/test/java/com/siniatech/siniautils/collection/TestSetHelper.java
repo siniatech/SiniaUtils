42ae3b68c07e6ce0ac7b9ba7a7bbc1c95ac97a04
si <si@sifraser.co.uk>
2012-05-22 20:28:02 +0100

    finish set helper test

--- siniautils/src/test/java/com/siniatech/siniautils/collection/TestSetHelper.java
+++ siniautils/src/test/java/com/siniatech/siniautils/collection/TestSetHelper.java
@@ -10,6 +10,8 @@ import java.util.Set;
 
 import org.junit.Test;
 
+import com.siniatech.siniautils.named.Named;
+
 public class TestSetHelper {
 
     @Test
@@ -40,10 +42,47 @@ public class TestSetHelper {
         assertContains( 3, intSet );
     }
 
+    @SuppressWarnings("cast")
     @Test
     public void emptySet_isZeroCardinalitySet() throws Exception {
         assertEquals( 0, emptySet().size() );
         assertTrue( emptySet() instanceof Set );
+    }
+
+    @Test
+    public void union_failsOnNull_p1() throws Exception {
+        try {
+            union( asSet( "a" ), null );
+            fail();
+        } catch ( Exception e ) {
+        }
+    }
+
+    @Test
+    public void union_failsOnNull_p2() throws Exception {
+        try {
+            union( null, asSet( "a" ) );
+            fail();
+        } catch ( Exception e ) {
+        }
+    }
+
+    @Test
+    public void union_obj() throws Exception {
+        Named a = new Named( "a" );
+        Named b = new Named( "b" );
+        assertEquals( asSet( a, b ), union( asSet( a ), asSet( b ) ) );
+        assertEquals( asSet( a, b ), union( asSet( a, b ), SetHelper.<Named> asSet() ) );
+        assertEquals( asSet( a, b ), union( SetHelper.<Named> asSet(), asSet( a, b ) ) );
+        assertEquals( asSet( a, b ), union( asSet( a, b ), asSet( a, b ) ) );
+    }
+
+    @Test
+    public void union_primitive() throws Exception {
+        assertEquals( asSet( 1, 2 ), union( asSet( 1 ), asSet( 2 ) ) );
+        assertEquals( asSet( 1, 2 ), union( asSet( 1, 2 ), SetHelper.<Integer> asSet() ) );
+        assertEquals( asSet( 1, 2 ), union( SetHelper.<Integer> asSet(), asSet( 1, 2 ) ) );
+        assertEquals( asSet( 1, 2 ), union( asSet( 1, 2 ), asSet( 1, 2 ) ) );
     }
 
 }
