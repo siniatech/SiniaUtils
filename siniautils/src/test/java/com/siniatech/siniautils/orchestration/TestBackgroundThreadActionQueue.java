ab4725b403896ae34d99e6a8911a7ea3afb01872
si <si@sifraser.co.uk>
2012-05-29 13:17:08 +0100

    license/headers

--- siniautils/src/test/java/com/siniatech/siniautils/orchestration/TestBackgroundThreadActionQueue.java
+++ siniautils/src/test/java/com/siniatech/siniautils/orchestration/TestBackgroundThreadActionQueue.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * SiniaUtils
+ * Copyright (c) 2011-2 Siniatech Ltd  
+ * http://www.siniatech.com/products/siniautils
+ *
+ * All rights reserved. This project and the accompanying materials are made 
+ * available under the terms of the MIT License which can be found in the root  
+ * of the project, and at http://www.opensource.org/licenses/mit-license.php
+ *
+ ******************************************************************************/
 package com.siniatech.siniautils.orchestration;
 
 import static junit.framework.Assert.*;
