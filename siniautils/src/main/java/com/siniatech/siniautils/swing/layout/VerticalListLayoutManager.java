4a975f109addc0cf4d5903bd4ccd27da147207a3
Si Fraser <simon@siniatech.com>
2013-04-27 11:52:41 +0100

    Get vertical layout manager working properly

--- siniautils/src/main/java/com/siniatech/siniautils/swing/layout/VerticalListLayoutManager.java
+++ siniautils/src/main/java/com/siniatech/siniautils/swing/layout/VerticalListLayoutManager.java
@@ -3,14 +3,22 @@ package com.siniatech.siniautils.swing.layout;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Insets;
 import java.awt.LayoutManager;
+
+import javax.swing.JComponent;
+import javax.swing.border.Border;
 
 public class VerticalListLayoutManager implements LayoutManager {
 
-    private final int listItemHeight;
+    private static final Insets EMPTY_INSETS = new Insets( 0, 0, 0, 0 );
+    private final int hGap;
 
-    public VerticalListLayoutManager( int listItemHeight ) {
-        this.listItemHeight = listItemHeight;
+    public VerticalListLayoutManager() {
+        this(0);
+    }
+    public VerticalListLayoutManager(int hGap) {
+        this.hGap = hGap;
     }
 
     @Override
@@ -23,21 +31,45 @@ public class VerticalListLayoutManager implements LayoutManager {
 
     @Override
     public Dimension preferredLayoutSize( Container parent ) {
-        return new Dimension( 100, listItemHeight * parent.getComponents().length );
+        Insets borderInsets = getBorderInsets( parent );
+        int w = parent.getWidth();
+        Component[] components = parent.getComponents();
+        int h = borderInsets.top + borderInsets.bottom;
+        for ( int i = 0; i < components.length; i++ ) {
+            h += components[i].getPreferredSize().height;
+        }
+        return new Dimension( w, h );
     }
 
     @Override
     public Dimension minimumLayoutSize( Container parent ) {
-        return new Dimension( 0, 0 );
+        return preferredLayoutSize( parent );
     }
 
     @Override
     public void layoutContainer( Container parent ) {
-        int w = parent.getWidth();
+        Insets borderInsets = getBorderInsets( parent );
+        int x = borderInsets.left;
+        int w = parent.getWidth() - x - borderInsets.right;
+        int y = borderInsets.top;
         Component[] components = parent.getComponents();
         for ( int i = 0; i < components.length; i++ ) {
-            components[i].setBounds( 0, i * listItemHeight, w, listItemHeight );
+            Component component = components[i];
+            int h = component.getPreferredSize().height;
+            component.setBounds( x, y, w, h );
+            y += h;
+            y += hGap;
         }
+    }
+
+    private Insets getBorderInsets( Component component ) {
+        if ( component instanceof JComponent ) {
+            Border border = ( (JComponent) component ).getBorder();
+            if ( border != null ) {
+                return border.getBorderInsets( component );
+            }
+        }
+        return EMPTY_INSETS;
     }
 
 }
